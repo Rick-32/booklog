3ee52cfb6f942710b36752a93ce4c6a5dde0429c
Rick-32 <okano.0407@gmail.com>
2019-02-18 20:47:08 +0900

    認証をemailからuser_idに変更するための記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+	#before_filterを設定
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		#strong parametersを設定し、usernameを許可
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :use_id, :password, :password_confirmation])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:user_id, :password, :remember_me])
+	end
 end
