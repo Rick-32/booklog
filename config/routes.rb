054504d81f9377a4cce7a7ebaadb49eff01061a3
Rick-32 <okano.0407@gmail.com>
2019-03-09 13:42:49 +0900

    新規登録の際にconfirmation用のメールが送信される

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'books#index'
+  devise_for :users, :controllers => {
+		:registrations => 'users/registrations',
+		:sessions => 'users/sessions'
+	}
+
+	devise_scope :user do
+		get 'confirm_email', to: 'users/registrations#confirm_email'
+	end
+
+	resources :users
+	root 'books#index'
+	get 'users/:id' => "users#show", as: :home
+	get '/logout' => "users#logout", as: :logout
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_003540) do
+ActiveRecord::Schema.define(version: 2019_02_23_115138) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 2019_02_17_003540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_id", default: "", null: false
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
 
