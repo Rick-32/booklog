0ff98ba9516fa361e28f08dc75dec1d0de50efee
Rick-32 <okano.0407@gmail.com>
2019-03-21 21:59:49 +0900

    users_controllerでupdateアクションとeditアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,26 @@ layout 'users'
 	def edit
 		@user = User.find(params[:id])
 	end
+
+	def update
+		@user = current_user
+		respond_to do |format|
+			if @user.update(user_params)
+				format.html{ redirect_to controller: 'users', action: 'profile', notice: 'Profile was successfully updated.' }
+				format.json{ render :profile, status: :ok, location: @user }
+			else
+				format.html { render :edit }
+				format.json { render json: @user.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+private
+	def user_params
+		params.require(:user).permit(:user_id, :name, :gender, :bookshelf_name, :prefecture, :hometown, :occupation, :profile, :bookshelf_profile, :birth_y, :birth_m, :birth_d)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
+	end
 end
