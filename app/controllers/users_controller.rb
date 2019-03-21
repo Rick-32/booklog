5abe81d6a63e3816f090d27ea040bce6eeeda59c
Rick-32 <okano.0407@gmail.com>
2019-03-21 12:11:27 +0900

    プロフール画面の表示、編集でのアクション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,13 @@ class UsersController < ApplicationController
 layout 'users'
 
 	def home
-
 	end
 
 	def profile
+		@user = current_user
+	end
+
+	def edit
+		@user = User.find(params[:id])
 	end
 end
