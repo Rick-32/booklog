8965c2883d6a8232da43d940a854a1bab9c258c2
Rick-32 <okano.0407@gmail.com>
2019-03-23 12:57:52 +0900

    before＿actionでフィルターとしてセットしたset_userメソッドを記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,17 @@
 class UsersController < ApplicationController
 layout 'users'
+before_action :set_user, only: [:profile, :edit, :update, :destroy]
 
 	def home
 	end
 
 	def profile
-		@user = current_user
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = current_user
 		respond_to do |format|
 			if @user.update(user_params)
 				format.html{ redirect_to controller: 'users', action: 'profile', notice: 'Profile was successfully updated.' }
@@ -26,11 +24,11 @@ layout 'users'
 	end
 
 private
-	def user_params
-		params.require(:user).permit(:user_id, :name, :gender, :bookshelf_name, :prefecture, :hometown, :occupation, :profile, :bookshelf_profile, :birth_y, :birth_m, :birth_d)
-	end
 
 	def set_user
-		@user = User.find(params[:id])
+		@user = User.find(current_user.id)
+	end
+	def user_params
+		params.require(:user).permit(:user_id, :name, :gender, :bookshelf_name, :prefecture, :hometown, :occupation, :profile, :bookshelf_profile, :birth_y, :birth_m, :birth_d)
 	end
 end
