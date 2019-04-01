749ccfb69d2bdf13071d71a2f9d51587ab100cd3
Rick-32 <okano.0407@gmail.com>
2019-04-01 20:00:52 +0900

    本の検索、結果一覧表示、さらにこのあと実装予定の本の詳細画面のためのshowアクションを定義/books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,5 +2,14 @@ class BooksController < ApplicationController
 layout 'books'
 
 	def index
+		@books = Book.order('id ASC').limit(10)
+	end
+
+	def show
+		@book = Book.find(params[:id])
+	end
+
+	def search
+		@books = Book.where('title LIKE(?)', "%#{params[:keyword]}%").limit(10)
 	end
 end
