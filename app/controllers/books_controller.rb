abff8bc40c5b64d3440dc3d8e4474358014dfaef
Rick-32 <okano.0407@gmail.com>
2019-04-30 03:12:54 +0900

    本の登録アクションの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,4 +12,20 @@ layout 'books'
 	def search
 		@books = Book.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(10)
 	end
+
+	def register
+		@book = Book.find(params[:id])
+		@book.users << current_user
+		redirect_to action: :search
+	end
+
+	private
+
+	def set_shelfrecord
+		@user = current_user
+		@book = book(params[:id])
+	end
+	def books_params
+		params.require(:BooksUser).permit(:user_id, :book_id).merge(book_id: params[:book_id])
+	end
 end
